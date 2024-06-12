bd2193898d22e3d86d9577c66114f3c518d5ad72
Emyowo204 <egaete2023@inf.udec.cl>
2024-06-11 22:54:00 -0400

    Agregación de todos los animales en la clase AnimalFactory

--- ProyectoFinal/src/main/java/Modelos/Utils/AnimalFactory.java
+++ ProyectoFinal/src/main/java/Modelos/Utils/AnimalFactory.java
@@ -1,9 +1,12 @@
 package Modelos.Utils;
 
 import Modelos.Bosque.*;
-import Modelos.Acuatico.*;
-import Modelos.Enumeration.*;
 import Modelos.Sabana.*;
+import Modelos.Selva.*;
+import Modelos.Acuatico.*;
+import Modelos.Desierto.*;
+import Modelos.Artico.*;
+import Modelos.Enumeration.*;
 
 public class AnimalFactory {
     public Animal crearAnimal(TipoAnimal tipo, String nombre) {
@@ -35,7 +38,21 @@ public class AnimalFactory {
             } else {
                 return new Leon(nombre);
             }
-        } else {
+        } else if(tipo.getHabitat() == TipoHabitat.Selva) {
+            if (tipo == TipoAnimal.Capibara) {
+                return new Capibara(nombre);
+            } else if (tipo == TipoAnimal.Cocodrilo) {
+                return new Cocodrilo(nombre);
+            } else if (tipo == TipoAnimal.Loro) {
+                return new Loro(nombre);
+            } else if (tipo == TipoAnimal.Mono) {
+                return new Mono(nombre);
+            } else if (tipo == TipoAnimal.OsoHormiguero) {
+                return new OsoHormiguero(nombre);
+            } else {
+                return new Serpiente(nombre);
+            }
+        } else if(tipo.getHabitat() == TipoHabitat.Acuatico) {
             if (tipo == TipoAnimal.Coipo) {
                 return new Coipo(nombre);
             } else if (tipo == TipoAnimal.Delfin) {
@@ -48,6 +65,34 @@ public class AnimalFactory {
                 return new Tiburon(nombre);
             } else {
                 return new Tortuga(nombre);
+            }
+        } else if(tipo.getHabitat() == TipoHabitat.Desierto) {
+            if (tipo == TipoAnimal.Avestruz) {
+                return new Avestruz(nombre);
+            } else if (tipo == TipoAnimal.Camello) {
+                return new Camello(nombre);
+            } else if (tipo == TipoAnimal.Canguro) {
+                return new Canguro(nombre);
+            } else if (tipo == TipoAnimal.Coyote) {
+                return new Coyote(nombre);
+            } else if (tipo == TipoAnimal.Lagarto) {
+                return new Lagarto(nombre);
+            } else {
+                return new Suricata(nombre);
+            }
+        } else {
+            if (tipo == TipoAnimal.Alce) {
+                return new Alce(nombre);
+            } else if (tipo == TipoAnimal.BuhoNival) {
+                return new BuhoNival(nombre);
+            } else if (tipo == TipoAnimal.Morsa) {
+                return new Morsa(nombre);
+            } else if (tipo == TipoAnimal.OsoPolar) {
+                return new OsoPolar(nombre);
+            } else if (tipo == TipoAnimal.Pinguino) {
+                return new Pinguino(nombre);
+            } else {
+                return new ZorroArtico(nombre);
             }
         }
     }
